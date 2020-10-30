c49fb0620b2def058947a7206264a4097f5568d8
Kamil Łobasiuk <Kamil.lobas@gmail.com>
2020-10-30 14:50:56 +0100

    start of project

---
+++ src/main/java/com/dungeonhunters/dungeonhunters/DungeonhuntersApplication.java
@@ -0,0 +1,13 @@
+package com.dungeonhunters.dungeonhunters;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DungeonhuntersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DungeonhuntersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dungeonhunters/dungeonhunters/DungeonhuntersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dungeonhunters.dungeonhunters;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DungeonhuntersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
