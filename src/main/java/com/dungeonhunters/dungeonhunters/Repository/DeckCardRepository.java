e159b22c2f47deab5a16788c7654760ce9ac19e3
Mateusz Bielecki <bielakb98@gmail.com>
2020-11-24 22:37:25 +0100

    small tweaks

--- src/main/java/com/dungeonhunters/dungeonhunters/Repository/DeckCardRepository.java
+++ src/main/java/com/dungeonhunters/dungeonhunters/Repository/DeckCardRepository.java
@@ -2,6 +2,7 @@ package com.dungeonhunters.dungeonhunters.Repository;
 
 
 import com.dungeonhunters.dungeonhunters.model.Card;
+import com.dungeonhunters.dungeonhunters.model.Deck;
 import com.dungeonhunters.dungeonhunters.model.DeckCard;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
@@ -20,4 +21,14 @@ public interface DeckCardRepository extends JpaRepository<DeckCard,Long> {
     @Modifying(clearAutomatically = true)
     @Query(value = "insert into Deck_Card (ID, CARD_ID,DECK_ID) VALUES (2,:idCard,:idDeck)", nativeQuery = true)
     void addCardToDeck(@Param("idCard") Long idCard, @Param("idDeck") Long idDeck);
+
+    @Transactional
+    @Modifying(clearAutomatically = true)
+    @Query("select c from Card c, Deck d where c.id=d.id and d.id=:id")
+    List<Card> getAllCardsFromDeck(@Param("id") Long id);
+
+    @Transactional
+    @Modifying(clearAutomatically = true)
+    @Query("delete from DeckCard dc where dc.deck=:deck and dc.card=:card")
+    void deleteCardFromDeck(@Param("deck") Deck deck, @Param("card") Card card);
 }
