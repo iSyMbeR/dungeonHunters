1731e7c350d33c8154b4ac47ca7150a921598ae7
Mateusz Bielecki <bielakb98@gmail.com>
2020-11-29 23:53:08 +0100

    enabled fullscreen mode

--- src/main/java/com/dungeonhunters/dungeonhunters/controller/GameController.java
+++ src/main/java/com/dungeonhunters/dungeonhunters/controller/GameController.java
@@ -51,7 +51,8 @@ public class GameController extends JFrame {
         this.fightController = fightController;
         this.panel = new JPanel();
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-        this.setSize(new Dimension(500, 500));
+        this.setExtendedState(JFrame.MAXIMIZED_BOTH);
+        this.setUndecorated(true);
         switchToMenuController();
 
     }
